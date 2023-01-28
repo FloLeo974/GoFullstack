82449f1e923cc54c666f35a990efd128e065f451
Florient Leonard <florient_leonard@hotmail.com>
2023-01-28 23:30:43 +0400

    Ajout de la modification

--- app.js
+++ app.js
@@ -28,6 +28,12 @@ app.post('/api/stuff', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
 app.get('/api/stuff/:id', (req, res, next) => { // : => indique que la partie de la route qui suit est dynamique
   Thing.findOne({ _id: req.params.id }) // findOne => trouve un seul // _id est le même que req.params.id
     .then(thing => res.status(200).json(thing))
