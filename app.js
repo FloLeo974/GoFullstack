b694312dc809efed5e2367e8fe1791cdb105826e
Florient Leonard <florient_leonard@hotmail.com>
2023-02-01 13:26:18 +0400

    Créer des users et vérifier les infos d'auth.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const stuffRoutes = require('./routes/stuff');
-
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://Flo974:mongoDB974@cluster0.jiqgava.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -20,5 +20,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
